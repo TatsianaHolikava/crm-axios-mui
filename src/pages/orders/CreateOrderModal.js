4d43396df5dba93b14fc9ee5d5498afa3d653343
TatsiH <gtaneka@gmail.com>
2023-03-08 14:35:46 -0500

    orders in progress

---
+++ src/pages/orders/CreateOrderModal.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import "../ModalWindow.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CreateClientModal(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [newName, setNewName] = useState("")
+  const [newAddress, setNewAddress] = useState("")
+  const [newPhoneNumber, setNewPhoneNumber] = useState("")
+  const [newCreateAt, setNewCreateAt] = useState("")
+ 
+
+//   const createClient = (event) => {
+//     event.preventDefault();
+//     const clientObj = {
+//       name: newName,
+//       address: newAddress,
+//       phoneNumber: newPhoneNumber,
+//       createAt: newCreateAt,
+
+//     };
+//     props.createClient(clientObj);
+//     handleClose();
+//   };
+
+
+  return (
+    <div>
+      <Button variant="contained" onClick={handleOpen}>
+        Create Client
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Create new client
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <TextField
+              sx={{ marginTop: "5px", width: "300px" }}
+              id="outlined-basic"
+              label="Name:"
+              variant="outlined"
+            
+            />
+            <TextField
+              sx={{ marginTop: "5px", width: "300px" }}
+              id="outlined-basic"
+              label="Address:"
+              variant="outlined" 
+            
+            />
+            <TextField
+              sx={{ marginTop: "5px", width: "300px" }}
+              id="outlined-basic"
+              label="Phone number:"
+              variant="outlined"
+             
+            />
+            <TextField
+              sx={{ marginTop: "5px", width: "300px" }}
+              id="outlined-basic"
+              variant="outlined"  
+              type="date"
+            
+            />
+           
+          </Typography>
+          <div className="actions">
+            <Button
+              className="cancel-btn"
+              variant="outlined"
+              sx={{
+                backgroundColor: "red",
+                color: "white",
+                marginRight: "5px",
+              }}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button variant="contained" >
+              Create client
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import Button from "@mui/material/Button";
+import CreateOrderModal from './CreateOrderModal'
+import DeleteOrderModal from './DeleteOrderModal'
+import './Orders.css'
 
-const Orders = () => {
-    return (
-        <div>
-            <h1>Orders</h1>
+const Orders = (props) => {
+  return (
+    <div>
+   
+      <div className="clients-table">
+        <div className="header">
+        <h1>Orders</h1>
+           <CreateOrderModal /> 
         </div>
-    );
+        <table>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>Name</th>
+              <th>Service</th>
+              <th>Price</th>
+              <th>Payments</th>
+              <th>Debt</th>
+              <th>Created at</th>
+              <th>Status</th>
+              <th>Dates</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {/* {props.clients.map((client) => ( */}
+              <tr >
+                <td>1</td>
+                <td>John</td>
+                <td>cleaning</td>
+                <td>120</td>
+                <td>50</td>
+                <td>70</td>
+                <td>2023-03-08</td>
+                <td>In progress</td>
+                <td>2023-03-08</td>
+                <td>
+                  <div class="dropdown">
+                    <Icon path={mdiMenu} size={1} />
+
+                    <div class="dropdown-content">
+                      <Button variant="outlined" sx={{ color: "red" }}>
+                       <DeleteOrderModal/>
+                      </Button>
+                      {/* <EditClientModal updateClient={props.updateClient} client={client}/> */}
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            {/* ))} */}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
